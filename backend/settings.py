3a219b02ef50f0be1d27b57cf48bd585de070e58
nathanteyou <teyou21@gmail.com>
2024-06-22 20:13:57 +0100

    Updated the images and text

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "Contoso"
+    title: str = "Ophelia Chat"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
-    chat_title: str = "Start chatting"
-    chat_description: str = "This chatbot is configured to answer your questions"
+    chat_title: str = "Ask Ophelia"
+    chat_description: str = "I am your MTN virtual assistant. How can I help you ?"
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
 
